53b00f25b045ea49929425e28aa0855e92a3faa5
fxsym <fsyamsudin24@gmail.com>
2025-03-29 00:59:22 +0700

    Setup buat deploy ke github

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
             <nav className="bg-sky-100">
                 <div className="flex items-center justify-center">
                     <h1 className="text-3xl font-[Roboto_Condensed] p-3">Render-Quotes</h1>
+                    <p>Using React + Vite + TailwindCSS</p>
                 </div>
             </nav>
         </>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/web-api-jagokata",
   build: {
     rollupOptions: {
       input: {
