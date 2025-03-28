54d201fd9ac937710380da2e5c93d3b9858acefc
fxsym <fsyamsudin24@gmail.com>
2025-03-28 20:49:59 +0700

    Berhasil fetch data menggunakan axios tetapi dengan link tambahan karena CORS rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './Header/Navbar'
 import Hero from './Header/Hero'
 import Body from './Body/Body'
+import axios from "axios";
 
 function App() {
+const [data, setData] = useState(null)
+const [error, setError] = useState(null)
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://api.allorigins.win/raw?url=https://jagokata-api.vercel.app/popular")
+      setData(response.data)
+    } catch (error) {
+      setError(error)
+    }
+  }
+  fetchData()
+}, [])
 
   return (
     <>
