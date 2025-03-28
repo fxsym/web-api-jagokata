a62ae15247778abde21d67a419bbe49480f34848
fxsym <fsyamsudin24@gmail.com>
2025-03-29 01:16:52 +0700

    Menambahkan AOS Scroll Animation

--- src/Body/Card.jsx
+++ src/Body/Card.jsx
@@ -1,8 +1,16 @@
+import React,{useEffect} from "react";
 import { useQuery } from "../context/QueryContext";
 import useFetchData from "../util/useFetchData";
 import useSearchQuote from "../util/useSearchQuote";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Card() {
+
+    useEffect(() => {
+        AOS.init();
+      }, [])
+
     const {query} = useQuery()
     const { data: initialData, error: fetchError } = useFetchData();
     const { data: searchData, error: searchError } = useSearchQuote();
@@ -10,11 +18,13 @@ export default function Card() {
     const quotes = query ? searchData?.data?.quotes : initialData?.data?.quotes;
     if (!quotes) return <p>Loading...</p>;
 
+    
+
     return (
         <>
             <div className="mx-auto font-[Poppins] lg:flex lg:flex-wrap lg:gap-6 justify-evenly">
                 {quotes.map((quote, index) => (
-                <div className="flex bg-sky-100 rounded-2xl shadow-lg shadow-sky-300 mb-6 overflow-hidden items-center p-6 gap-6 cursor-pointer hover:scale-110 transition-all duration-300 w-[100%] lg:w-[45%] xl:w-[30%]" key={index}>
+                <div data-aos="zoom-in" className="flex bg-sky-100 rounded-2xl shadow-lg shadow-sky-300 mb-6 overflow-hidden items-center p-6 gap-6 cursor-pointer hover:scale-110 transition-all duration-300 w-[100%] lg:w-[45%] xl:w-[30%]" key={index}>
                     <img src={quote.author.img} alt="" className="rounded-full w-25 h-25 aspect-square object-cover"/>
                     <div className="">
                         <p className="text-justify text-2xs font-[Oswald]">"{quote.quote}"</p>
--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -4,7 +4,6 @@ export default function Navbar() {
             <nav className="bg-sky-100">
                 <div className="flex items-center justify-center">
                     <h1 className="text-3xl font-[Roboto_Condensed] p-3">Render-Quotes</h1>
-                    <p>React + Vite + TailwindCSS</p>
                 </div>
             </nav>
         </>
