4ce9c18df0429d4fb13dd7417f3ce49840d344e6
farihsyamsudin <farihsyamsudin31@upi.edu>
2025-03-29 10:29:16 +0700

    [ADD] Loading & Handler Error

--- src/util/useFetchData.jsx
+++ src/util/useFetchData.jsx
@@ -4,20 +4,25 @@ import axios from "axios";
 export default function useFetchData() {
   const [data, setData] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
-          "https://api.allorigins.win/raw?url=https://jagokata-api.vercel.app/popular"
+          "https://jagokata-api.vercel.app/popular"
         );
         setData(response.data);
       } catch (error) {
+        console.error(error);
         setError(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
-  return { data, error };
+  return { data, error, loading };
 }
